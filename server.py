6bec07207415bcd50718789b0b6c38e77a0b8754
Ezaan Mangalji <emangalji@gmail.com>
2019-04-29 03:32:05 -0400

    simple page setup

--- server.py
+++ server.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def splash():
-  return "Hello World!"
+  return render_template("splash.html")
+  
+@app.route("/fight", methods=["GET"])
+def fight():
+  return render_template("fight.html")
+
+@app.route("/test", methods=["GET"])
+def test():
+  return render_template("test.html")
+
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
