3f263687a190ab0c117b6be7aa0a82b2b6f5b16f
Ezaan Mangalji <emangalji@gmail.com>
2019-04-29 04:04:24 -0400

    image uploading

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def splash():
@@ -13,6 +17,19 @@ def fight():
 @app.route("/test", methods=["GET"])
 def test():
   return render_template("test.html")
+  
+@app.route("/test_upload", methods=["POST"])
+def test_upload():
+  file = request.files['image']
+  f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+  
+  file.save(f)
+  
+#  do some ML on the file and get classification
+  
+  classification = "100p a hot dog"
+  
+  return render_template("test.html", classification = classification)
 
 
 if __name__ == "__main__":
