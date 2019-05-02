20e4752b7da64357533f47968f839f50f73f32bb
Ezaan Mangalji <emangalji@gmail.com>
2019-05-01 21:31:41 -0400

    shuffling of images and fight image upload

--- server.py
+++ server.py
@@ -11,22 +11,36 @@ from keras import backend as K
 
 app = Flask(__name__)
 
+fightPhotos = []
+fightLabels = ["hotdogs", "hotdogs", "hotdogs", "hotdogs", "legs", "legs", "legs", "legs"] 
+
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def splash():
   return render_template("splash.html")
+
+def shuffleFight():
+  p = np.random.permutation(8)
+  global fightPhotos
+  global fightLabels
+  fightPhotos = np.array(fightPhotos)
+  fightLabels = np.array(fightLabels)
+  fightPhotos = fightPhotos[p]
+  fightLabels = fightLabels[p]
   
 @app.route("/fight", methods=["GET"])
 def fight():
+  shuffleFight()
   return render_template("fight.html")
   
 @app.route("/fightPlay", methods=["POST"])
 def fightPlay():
-  index = request.form.get('index')
-  correct = request.form.get('correct')
-  return render_template("fightStart.html")
+  print(fightPhotos)
+  index = int(request.form.get('index'))
+  correct = int(request.form.get('correct'))
+  return render_template("fightStart.html", imageIndex=index+1, imageLink="", score=correct)
 #  return render_template("fight.html", imageIndex=xxx, imageLink=, correct=)
 
 @app.route("/test", methods=["GET"])
@@ -38,11 +52,15 @@ def test_upload():
   file = request.files['image']
   filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
   
-  file.save(f)
+  file.save(filename)
   
 #  do some ML on the file and get classification
   
   classification = "100p a hot dog"
+  
+  os.remove(filename)
+  
+  return render_template("test.html", classification = classification)
 
   #call predict with the image route from wherever u need to get the response
   #"hotdogs" or "legs"
@@ -67,10 +85,8 @@ def predict(img_url):
   
 #  once classified, delete image
   
-  os.remove(filename)
-  
-  return render_template("test.html", classification = classification)
-
-
 if __name__ == "__main__":
+  fightPhotos = [f for f in os.listdir("static/") if os.path.isfile(os.path.join("static/", f))]
+  fightPhotos.remove(".DS_Store")
+  fightPhotos.sort()
   app.run(port=5000, debug=True)
